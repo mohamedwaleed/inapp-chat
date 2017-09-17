0d96d8304d2fc57e9fa528b4f3f64cddbd2c0989
Mohamed Wlaeed <mohamedwlaeed2012@gmail.com>
2017-09-17 07:45:54 +0200

    add small-client

---
+++ small-client/chat-sdk.js
@@ -0,0 +1,26 @@
+var inAppChat = (function inAppChat(argument) {
+		
+	const BASE_URL = 'http://localhost:3000';
+
+	function __init__(params, messageCallback, userIdCallback) {
+
+		  const email = params.email;
+		  const appId = params.appId;
+		  const isClient = true;
+		  var myId = -1;
+		  var socket = io(BASE_URL,{query: 'app_id='+appId+'&user_email=' + email + '&is_client=' + isClient});
+
+		  socket.on('user_id', function (userId) {
+		  	  myId = userId;
+		  	  userIdCallback(userId);
+		  	  socket.on(myId, function (message) {
+			  	messageCallback(message);
+			  });
+		  });
+	}
+
+	return {
+		init: __init__
+	};
+
+})();
